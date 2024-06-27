d9062fcb221742c9c8885dc12771d220ccb739d3
Beat <75857504+BeatACVR@users.noreply.github.com>
2024-06-27 17:33:16 -0500

    Create install-requirements.bat

---
+++ KeySpammer/install-requirements.bat
@@ -0,0 +1,11 @@
+pip install --upgrade keyboard
+@echo off
+echo.
+echo.  
+echo Requirements are now installed.
+echo.
+echo Please edit the run.py file to change the configuration. The default configuration is:
+echo Key to hold: F6
+echo Key to spam: Spacebar
+echo Delay in seconds: 0.01
+pause
